7488fb57e13bbb187835e689be1ad77d09afb8cc
Sergionx <sergio180203@gmail.com>
2023-09-12 17:05:16 -0400

    xd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const { data: posts } = await supabase.from("posts").select("*");
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       mola
